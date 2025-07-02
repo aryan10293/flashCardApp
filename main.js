d10b1523de04f094f55cc9b088b5f13dd25320aa
aondrej ryan <aondrejryan11@gmail.com>
2025-07-02 13:42:01 -0700

    i got a little better today

--- main.js
+++ main.js
@@ -1,28 +1,14 @@
-// i needed an Api and i dont have interent rn
-const flashCards = {
-    1: {
-        "questions": "How many players are on a basketball court in a standard game?",
-        "answer": "10",
-    },
-    2: {
-        "questions": "what is it called when a player takes to many steps without dirbbling the ball?",
-        "answer": "travel",
-    },
-    3: {
-        "questions": "how many points is it worth for a shot taken inside the three point line?",
-        "answer": "2 points",
-    },
-    4: {
-        "questions": 'which nba player has the nickname "King James"?',
-        "answer": "Lebron James",
-    },
-    5: {
-        "questions": "what is the maximum numbers of fouls a player can have before fouling out of a high school basketball game?",
-        "answer": "5",
-    },
-};
+const flashCards = [
+    ["How many players are on a basketball court in a standard game?", "10"],
+    ["what is it called when a player takes to many steps without dirbbling the ball?", "travel"],
+    ["how many points is it worth for a shot taken inside the three point line?", "2 points"],
+    ['which nba player has the nickname "King James"?', "Lebron James"],
+    ["what is the maximum numbers of fouls a player can have before fouling out of a high school basketball game?", "5"]
+];
 
-const subjects = {};
+const subjects = {
+    testing: flashCards,
+};
 
 // interactive varibles
 const question = document.getElementById("question");
@@ -40,16 +26,22 @@ const select = document.getElementById("subjectSelect");
 const displaySubject = document.getElementById("displaySubject");
 const enter = document.getElementById("displaySubject");
 const deck = document.getElementById("deck");
+let currentDeck = "testing";
 
 
-let questionCount = 1;
-let currentQuestion = flashCards[questionCount];
-
+let questionCount = 0;
+let currentQuestion = subjects["testing"];
 const loadFirstQuestion = () => {
-    question.textContent = `Question: ${currentQuestion.questions}`;
+    if (currentQuestion.length === 0) {
+        question.textContent = "add a fucking question";
+    } else {
+        question.textContent = `Question: ${currentQuestion[questionCount][0]}`;
+    }
+
+
 }
 const showAnswer = () => {
-    answer.textContent = `Answer: ${currentQuestion.answer}`;
+    answer.textContent = `Answer: ${currentQuestion[questionCount][1]}`;
     answer.classList.toggle("hide");
     if (answer.classList.contains("hide")) {
         show.textContent = "Show Answer!";
@@ -59,50 +51,51 @@ const showAnswer = () => {
 
 }
 const backQuestion = () => {
-    if (questionCount === 1) {
-        questionCount = Object.keys(flashCards).length;
-        currentQuestion = flashCards[questionCount];
+    if (questionCount === 0) {
+        questionCount = currentQuestion.length - 1;
     } else {
         questionCount--;
-        currentQuestion = flashCards[questionCount];
     }
     loadFirstQuestion();
-    showAnswer();
+    if (!answer.classList.contains("hide")) {
+        showAnswer();
+    }
 
 }
 const nextQuestion = () => {
-    if (questionCount === Object.keys(flashCards).length) {
-        questionCount = 1;
-        currentQuestion = flashCards[questionCount];
+    if (questionCount === currentQuestion.length - 1) {
+        questionCount = 0;
     } else {
         questionCount++;
-        currentQuestion = flashCards[questionCount];
     }
     loadFirstQuestion();
-    showAnswer();
+    if (!answer.classList.contains("hide")) {
+        showAnswer();
+    }
 
 }
 const submitFlashcard = () => {
     const questionValue = addQuestion.value;
-    const answerValue = addAnswer.value
-    const lengthOfFlashCardSet = Object.keys(flashCards).length + 1
-    flashCards[lengthOfFlashCardSet] = {
-        "questions": questionValue,
-        "answer": answerValue
+    const answerValue = addAnswer.value;
+    if (subjects[currentDeck]) {
+        subjects[currentDeck].push([questionValue, answerValue]);
+        console.log(subjects);
     }
     addAnswer.value = " ";
     addQuestion.value = " ";
+    loadFirstQuestion();
 }
 
 const addAndDisplaySubject = () => {
     const option = document.createElement("option");
-    subjects[addSubject.value] = {};
+    subjects[addSubject.value.trim()] = [];
     option.innerHTML = addSubject.value;
     addSubject.value = " ";
     select.append(option);
     console.log(subjects)
 }
-
+// array 0 is the question 
+// array 1 is the answer
 
 next.addEventListener("click", nextQuestion);
 back.addEventListener("click", backQuestion);
@@ -110,7 +103,11 @@ show.addEventListener("click", showAnswer);
 submit.addEventListener("click", submitFlashcard);
 addDeck.addEventListener("click", addAndDisplaySubject)
 enter.addEventListener("click", (e) => {
-    deck.innerHTML = deck.textContent + " to " + select.value + " deck";
+    currentDeck = select.value;
+    deck.innerHTML = "Add a flashcard " + " to " + select.value + " deck";
+    currentQuestion = subjects[select.value];
+    questionCount = 0
+    loadFirstQuestion();
     console.log(select.value);
 })
 loadFirstQuestion();
